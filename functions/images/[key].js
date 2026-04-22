47cd089a0101075835b4dd808a3a431487189965
prinol <kimyonghee@gmail.com>
2026-04-23 03:09:52 +0900

    Add files via upload

--- functions/images/[key].js
+++ functions/images/[key].js
@@ -1,15 +1,12 @@
-export async function onRequestGet(context) {
-  const { env, params } = context;
-  const key = params.key;
-  if (!key) return new Response('Not found', { status: 404 });
-
-  const object = await env.ART_BUCKET.get(key);
-  if (!object) return new Response('Not found', { status: 404 });
+export async function onRequestGet({ params, env }) {
+  const object = await env.ART_BUCKET.get(params.key);
+  if (!object) {
+    return new Response('Not found', { status: 404 });
+  }
 
   const headers = new Headers();
   object.writeHttpMetadata(headers);
   headers.set('etag', object.httpEtag);
-  headers.set('cache-control', 'public, max-age=31536000, immutable');
-
+  headers.set('cache-control', 'public, max-age=3600');
   return new Response(object.body, { headers });
 }
